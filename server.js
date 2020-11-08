d71fb7545e65e677340237ec1d5d1b8e3e94c49a
IGOR <igornesan@gmail.com>
2020-11-08 19:41:27 +0200

    second chunk of code

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ const path = require('path');
 
 const PORT = 5000;
 
+//Chunk 2
+//Data parsing
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.json());
+
+app.post('/email', (req, res) => {
+  //@TODO
+  //send email here
+  console.log('Data: ', req.body);
+  res.json({ message: 'Message Received!!!' });
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
